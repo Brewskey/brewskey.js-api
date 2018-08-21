5814115b14bb38bccf4faaf0a24039b5f7a47afd
Anton puko <antonpukowebdev@gmail.com>
2018-08-21 18:14:44 +0200

    implement addFriend method, fix daoFetch

--- src/dao/FriendDAO.js
+++ src/dao/FriendDAO.js
@@ -4,6 +4,7 @@ import type { Friend, FriendStatus } from '../index';
 import DAO from './DAO';
 import { DAO_ENTITIES } from '../constants';
 import DefaultTranslator from '../translators/DefaultTranslator';
+import daoFetch from '../fetch';
 
 export const FRIEND_STATUSES: { [key: string]: FriendStatus } = {
   APPROVED: 'Approved',
@@ -20,6 +21,17 @@ class FriendDAO extends DAO<Friend, Friend> {
         friendAccount: { select: ['id', 'phoneNumber', 'userName'] },
       },
       translator: new DefaultTranslator(),
+    });
+  }
+
+  addFriend(userNameOrEmail: string): Promise<*> {
+    return daoFetch('friends/Default.addByUserName()/', {
+      body: JSON.stringify({ userName: userNameOrEmail }),
+      headers: [
+        { name: 'Accept', value: 'application/json' },
+        { name: 'Content-Type', value: 'application/json' },
+      ],
+      method: 'post',
     });
   }
 }
--- src/fetch.js
+++ src/fetch.js
@@ -4,11 +4,8 @@ import type { Header } from './index';
 
 import oHandler from 'odata';
 
-export default (path: string, options: ?Object): Promise<*> => {
-  const {
-    endpoint,
-    headers: oheaders = [],
-  } = oHandler().oConfig;
+export default async (path: string, options: ?Object): Promise<*> => {
+  const { endpoint, headers: oheaders = [] } = oHandler().oConfig;
 
   if (!endpoint) {
     throw new Error('no-ohandler-endpoint');
@@ -19,18 +16,24 @@ export default (path: string, options: ?Object): Promise<*> => {
     headers.append(name, value),
   );
 
-  (options && options.headers || []).forEach(
-    ({ name, value }: Header): void =>
-    headers.append(name, value),
+  ((options && options.headers) || []).forEach(
+    ({ name, value }: Header): void => headers.append(name, value),
   );
 
-  return fetch(
-    `${endpoint}/${path}`,
-    { ...options, headers },
-  ).then((response: Object): Object => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response;
-  }).then((response: Object): Object => response.json());
+  const response = await fetch(`${endpoint}/${path}`, { ...options, headers });
+
+  let responseJson;
+  try {
+    responseJson = await response.json();
+  } catch (error) {
+    responseJson = null;
+  }
+
+  if (!response.ok) {
+    throw new Error(
+      (responseJson.error && responseJson.error.message) || 'Whoops! Error!',
+    );
+  }
+
+  return responseJson;
 };
