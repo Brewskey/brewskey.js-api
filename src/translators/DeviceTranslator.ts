84577f726a061120dce0fde9a41a4b131b2bcafb
John Kalberer <jlkalberer@gmail.com>
2026-04-19 15:24:45 -0700

    Revert changes to Device type

--- src/translators/DeviceTranslator.ts
+++ src/translators/DeviceTranslator.ts
@@ -4,13 +4,12 @@ import DefaultTranslator from './DefaultTranslator';
 
 class DeviceTranslator extends DefaultTranslator<Device, DeviceMutator> {
   fromApi(apiValue: Device): Device {
-    const rawLocation = apiValue.location;
-    const location =
-      rawLocation && rawLocation.isDeleted ? undefined : rawLocation;
     return {
       ...super.fromApi(apiValue),
-      location,
-      locationId: rawLocation?.id,
+      location:
+        apiValue.location && apiValue.location.isDeleted
+          ? undefined
+          : apiValue.location,
     };
   }
 
@@ -20,14 +19,13 @@ class DeviceTranslator extends DefaultTranslator<Device, DeviceMutator> {
     lastEdited: _2,
     lastEditedBy: _3,
     location,
-    locationId: persistedLocationId,
     organization: _4,
     temperature: _5,
     ...otherProps
   }: Device): DeviceMutator {
     return {
       ...otherProps,
-      locationId: (location && location.id) ?? persistedLocationId,
+      locationId: location && location.id,
     };
   }
 }
